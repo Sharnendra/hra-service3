9920fb167d5a4b8c5bc77158542038c8e57de368
767967 <767967@PC300680.cts.com>
2019-05-27 14:39:04 +0530

    APP2

---
+++ src/main/java/com/rnlic/hraapp/repository/OtpRepository.java
@@ -0,0 +1,16 @@
+package com.rnlic.hraapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.rnlic.hraapp.modal.OtpMaster;
+
+@Repository
+public interface OtpRepository extends JpaRepository<OtpMaster, Long>{
+
+	@Query("SELECT o FROM OtpMaster o WHERE o.sapCode = :sapCode and o.uuid =:uuid and o.otpValue =:otp_value and expireTime > CURRENT_TIMESTAMP()")
+	OtpMaster findByUserId(@Param("sapCode") String sapCode, @Param("uuid") String uuid, @Param("otp_value") int otp_value);
+
+}
---
+++ src/main/java/com/rnlic/hraapp/testcontroller/MessagingServiceController.java
@@ -0,0 +1,82 @@
+package com.rnlic.hraapp.testcontroller;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.rnlic.hraapp.bean.OtpDetailsReqBean;
+import com.rnlic.hraapp.bean.OtpValidationReqBean;
+import com.rnlic.hraapp.service.MessagingService;
+@RestController
+public class MessagingServiceController {
+	
+private static final org.slf4j.Logger log = LoggerFactory.getLogger(MessagingServiceController.class);
+	
+	@Autowired
+	private MessagingService messageService;
+	
+	@GetMapping("/getSmS")
+	public String startApp(){
+		return "APP IS RUNNING!!";
+	}
+	
+
+	@PostMapping("/sendSMS")
+    public ResponseEntity<String> sendSMSOnPhone(@RequestBody OtpDetailsReqBean otpDetailsReqBean) 
+	{
+		log.info("=================in sendSMSOnPhone line 1 ==============");
+		SimpleClientHttpRequestFactory clientHttpReq = new SimpleClientHttpRequestFactory();
+		log.info("=================in sendSMSOnPhone line 2 ==============");
+		Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy.cognizant.com", 6050));
+		log.info("=================in sendSMSOnPhone line 3 ==============");
+		clientHttpReq.setProxy(proxy);
+		log.info("=================in sendSMSOnPhone line 4 ==============");
+		RestTemplate restTemplate = new RestTemplate(clientHttpReq);
+		log.info("=================in sendSMSOnPhone line 5 ==============");
+		ResponseEntity<String> response=null;
+		try{
+			log.info("=================in sendSMSOnPhone try 1 ==============");
+			response=restTemplate.exchange(messageService.sendSMSOnPhone(otpDetailsReqBean),
+			HttpMethod.GET, getHeaders(),String.class);
+			log.info("=================in sendSMSOnPhone try 1 ==============");
+		}
+		catch (Exception ex){
+			System.out.println(ex);
+		}
+        return new ResponseEntity<String>(response.getBody(), HttpStatus.OK);
+    }
+	
+	
+	@RequestMapping(value="/validateOtp",method=RequestMethod.POST)
+	public ResponseEntity<String> generateOtp(@RequestBody OtpValidationReqBean otpdetails){
+		if(messageService.validateOtp(otpdetails)) {
+			return new ResponseEntity<String>("Otp successfully validated!!", HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<String>("Otp is Not valid!!", HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	private static org.springframework.http.HttpEntity<?> getHeaders() throws IOException {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+		return new org.springframework.http.HttpEntity<>(headers);
+	}
+
+}
